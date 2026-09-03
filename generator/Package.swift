df964eb4907bf348a8817aa7f9f6139172c45a1d
maximkrouk <id.maximkrouk@gmail.com>
2026-09-03 13:55:04 +0200

    fix: support compatible SwiftSyntax versions

--- generator/Package.swift
+++ generator/Package.swift
@@ -2,23 +2,6 @@
 
 import PackageDescription
 
-#if canImport(FoundationEssentials)
-import FoundationEssentials
-#else
-import Foundation
-#endif
-
-extension Version {
-	/// swift-syntax version, for example "600.0.0"
-	static var swiftSyntaxVersion: Self {
-		let env = ProcessInfo.processInfo.environment
-		let versionString = env["GENERATE_SWIFT_SYNTAX_VERSION"]
-		?? "603.0.0"
-		// ?? "Env GENERATE_SWIFT_SYNTAX_VERSION is missing"
-		return Version(stringLiteral: versionString)
-	}
-}
-
 let package = Package(
 	name: "swiftlang-keywords-package-generator",
 	platforms: [
@@ -37,7 +20,7 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/swiftlang/swift-syntax.git",
-			exact: .swiftSyntaxVersion
+			"509.0.0"..<"605.0.0"
 		),
 		.package(
 			url: "https://github.com/capturecontext/swiftlang-snippets.git",
